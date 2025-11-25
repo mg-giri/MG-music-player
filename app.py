5e9da0d88c0d35e2f34a61cfddcd46ea17bf776b
Giri MG <mgthecoder@gmail.com>
2025-11-25 22:08:39 +0530

    Use environment variable for YOUTUBE_API_KEY
    Updated YOUTUBE_API_KEY to use environment variable.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 
 # Replace with your own YouTube Data API v3 key
-YOUTUBE_API_KEY ="AIzaSyCbPdvJmfbJ0Er8PTrMSdiewjAQGMD2x88"
+YOUTUBE_API_KEY = os.getenv("   YOUTUBE_API_KEY")
 
 YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 
